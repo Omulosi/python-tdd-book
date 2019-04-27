935c7def9b2d84ece46c585ca1e653e541f038dc
Omulosi <mulongojohnpaul@gmail.com>
2019-04-27 22:56:26 +0300

    Helper method in functional test for checking presence of list item;

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool online to-do app. She goes
@@ -39,19 +44,25 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         # self.assertTrue(
         #         any(row.text == '1: Buy peacock feathers' for row in rows),
         #         f"New to-do item did not appear in table. Contents were: \n{table.text}"
         #         )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-        # There is still a text inviting her to add anothe ritem. She enters
+        # There is still a text inviting her to add another item. She enters
         # "Use peacock feathers to make a fly"
-        self.fail('Finish test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again with both items on the list
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.fail('Finish test!')
 
         # The site generates a unique URL for Edith -- there is some explanatory text
         # to that effect
